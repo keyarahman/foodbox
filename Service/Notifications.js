a8274b74511a1f3dafdc9e0c358443a3c1211750
Negar Sultana <keya91bd@gmail.com>
2021-09-04 22:12:19 +0600

    firebase

--- Service/Notifications.js
+++ Service/Notifications.js
@@ -31,6 +31,42 @@ import messaging from '@react-native-firebase/messaging';
      console.log("Failed", "No token received");
     }
     }
-   
+   else{
+      console.log(fcmToken)
+   }
   
+  }
+
+
+  export const notificationListener = async () => {
+
+
+    messaging().onNotificationOpenedApp(remoteMessage => {
+        console.log(
+          'Notification caused app to open from background state:',
+          remoteMessage.notification,
+        );
+        // navigation.navigate(remoteMessage.data.type);
+      });
+  messaging().onMessage(remoteMessage => {
+        console.log(
+          'Notification caused app to open from foeground state:',
+          remoteMessage.notification,
+        );
+        // navigation.navigate(remoteMessage.data.type);
+      });
+     
+      messaging()
+        .getInitialNotification()
+        .then(remoteMessage => {
+          if (remoteMessage) {
+            console.log(
+              'Notification caused app to open from quit state:',
+              remoteMessage.notification,
+            );
+            // setInitialRoute(remoteMessage.data.type); // e.g. "Settings"
+          }
+      
+        });
+   
   }
